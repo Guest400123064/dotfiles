6808c1a6089d43d81c2842914b62da2565c974f6
guest400123064 <lgfz1003@icloud.com>
2026-02-23 02:15:59 +0000

    switch logic for k8s pod

--- .config/nvim/lua/plugins/ext-lspconfig.lua
+++ .config/nvim/lua/plugins/ext-lspconfig.lua
@@ -1,20 +1,26 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        pyright = {
-          settings = {
-            python = {
-              analysis = {
-                autoSearchPaths = true,
-                typeCheckingMode = "off",
-                useLibraryCodeForTypes = true,
-              },
-            },
+  "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
+  opts = function(_, opts)
+    -- Use system pyright (pip/conda) when Mason hasn't installed it (e.g., k8s pods).
+    -- On the laptop where Mason manages pyright, this leaves mason enabled as default.
+    local mason_bin = vim.fn.stdpath("data") .. "/mason/bin/pyright-langserver"
+    local mason_has_pyright = vim.uv.fs_stat(mason_bin) ~= nil
+
+    opts.servers = opts.servers or {}
+    opts.servers.pyright = vim.tbl_deep_extend("force", opts.servers.pyright or {}, {
+      mason = mason_has_pyright,
+      settings = {
+        python = {
+          analysis = {
+            autoSearchPaths = true,
+            typeCheckingMode = "off",
+            useLibraryCodeForTypes = true,
           },
         },
       },
-    },
-  },
+    })
+
+    return opts
+  end,
 }
